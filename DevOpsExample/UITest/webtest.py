c3f3089f9414e41be8e81cb6ab09415e9968a344
bo-hu2 <bo.hu2@hpe.com>
2017-10-27 04:59:33 -0500

    updated UI tests

--- DevOpsExample/UITest/webtest.py
+++ DevOpsExample/UITest/webtest.py
@@ -3,6 +3,7 @@ from pyvirtualdisplay import Display
 from selenium.webdriver.common.keys import Keys
 import os
 
+print("start selenium test.......")
 display = Display(visible=0, size=(800, 600))
 display.start()
 
@@ -11,7 +12,18 @@ url = os.environ.get('devops_endpoint','http://localhost:8000/WebExample/')
 driver.get(url)
 
 html = driver.page_source
+
+print("test case 1 : assert Name exists")
 assert "Name" in html
+
+print("test case 2 : assert Balance exists")
+assert "Balance" in html
+
+print("test case 3 : assert Number exists")
+assert "Number" in html
+
+print("test case 4 : assert Is VIP exists")
+assert "Is VIP" in html
 
 print("selenium test completed!")
 
