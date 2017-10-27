5fc335c78c7d77b8b9a5afb89e824ed925e98677
bo-hu2 <bo.hu2@hpe.com>
2017-10-27 01:53:05 -0500

    update selenium tests for Linux

--- DevOpsExample/UITest/webtest.py
+++ DevOpsExample/UITest/webtest.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
+from pyvirtualdisplay import Display
 from selenium.webdriver.common.keys import Keys
 import os
+
+display = Display(visible=0, size=(800, 600))
+display.start()
 
 driver = webdriver.Firefox()
 url = os.environ.get('devops_endpoint','http://localhost:8080/WebExample/')
